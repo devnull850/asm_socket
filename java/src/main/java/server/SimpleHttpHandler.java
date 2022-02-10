ea4d57f4167bb4855ef9e35760ffa8eb603f19cc
devnull850 <dev.null850@gmail.com>
2022-02-10 12:31:32 -0500

    Modify blob for different delivery idea

--- java/src/main/java/server/SimpleHttpHandler.java
+++ java/src/main/java/server/SimpleHttpHandler.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class SimpleHttpHandler implements HttpHandler {
 
 	private static final String PATH = "res/blob";
-	private static final int BLOB_SIZE = 0xe4;
+	private static final int BLOB_SIZE = 0x31;
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
