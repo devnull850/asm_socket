21a287e836996f0cb7110f8ca6253fe20791adc1
devnull850 <dev.null850@gmail.com>
2022-02-12 16:43:00 -0500

    Replace 64 bit instructions with 32 bit where appropriate

--- asm/test.s
+++ asm/test.s
@@ -3,16 +3,16 @@
 	.globl _start
 
 _start:
-	movq	$0x401078,%r8
+	movl	$0x401078,%r8d
 	jmp	.L0
 .L1:
 	movb	(%r8),%r9b
 	rolb	$4,%r9b
 	xorb	$0x42,%r9b
 	movb	%r9b,(%r8)
-	addq	$1,%r8
+	addl	$1,%r8d
 .L0:
-	cmpq	$0x4010a0,%r8
+	cmpl	$0x4010a0,%r8d
 	jl	.L1
 	movl	$0x29,%eax
 	movl	$0x2,%edi
@@ -20,9 +20,9 @@ _start:
 	xorl	%edx,%edx
 	syscall
 	pushq	%rax
-	xorq	%rax,%rax
+	xorl	%eax,%eax
 	pushq	%rax
-	movq	$0x901f0002,%rax
+	movl	$0x901f0002,%eax
 	pushq	%rax
 	movl	$0x2a,%eax
 	movl	0x10(%rsp),%edi
@@ -31,11 +31,11 @@ _start:
 	syscall
 	movl	$1,%eax
 	movl	0x10(%rsp),%edi
-	movq	$0x401078,%rsi
+	movl	$0x401078,%esi
 	movl	$0x28,%edx
 	syscall
 	movl	$0x7d,0xfffffffffffffff0(%rsp)
-	movq	$0x401a00,0xfffffffffffffff8(%rsp)
+	movl	$0x401a00,0xfffffffffffffff8(%rsp)
 .L3:
 	xorl	%eax,%eax
 	movl	0x10(%rsp),%edi
