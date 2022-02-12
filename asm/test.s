db95a911279d39a2e965ba549c28a0b0b71c02ff
devnull850 <dev.null850@gmail.com>
2022-02-12 16:10:30 -0500

    Remove checking syscall return value for errors

--- asm/test.s
+++ asm/test.s
@@ -20,8 +20,6 @@ _start:
 	movl	$0x1,%esi
 	xorl	%edx,%edx
 	syscall
-	test	%eax,%eax
-	je	.L2
 	movl	%eax,0x18(%rsp)
 	xorq	%rax,%rax
 	pushq	%rax
@@ -32,36 +30,27 @@ _start:
 	leaq	(%rsp),%rsi
 	movl	$0x10,%edx
 	syscall
-	cmpl	$0xffffffff,%eax
-	je	.L2
 	movl	$1,%eax
 	movl	0x28(%rsp),%edi
 	movq	$0x401078,%rsi
 	movl	$0x28,%edx
 	syscall
-	cmpl	$0x28,%eax
-	jne	.L2
 	movl	$0x7d,0x18(%rsp)
 	movq	$0x401a00,0x10(%rsp)
-.L4:
+.L3:
 	xorl	%eax,%eax
 	movl	0x28(%rsp),%edi
 	movq	0x10(%rsp),%rsi
 	movl	0x18(%rsp),%edx
 	syscall
 	test	%eax,%eax
-	jle 	.L3
+	jle 	.L2
 	addl	%eax,0x10(%rsp)
 	subl	%eax,0x18(%rsp)
-	jg	.L4
-.L3:
+	jg	.L3
+.L2:
 	movl	$0x3,%eax
 	movl	0x28(%rsp),%edi
 	syscall
 	movl	$0x401a4c,%eax
 	call	*%rax
-.L2:
-	xorl	%edi,%edi
-	addq	$0x30,%rsp
-	movl	$0x3c,%eax
-	syscall
