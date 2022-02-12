480125390bd53392d4ef050519f713b4c8fd637b
devnull850 <dev.null850@gmail.com>
2022-02-12 16:24:20 -0500

    Use push to place values in stack

--- asm/test.s
+++ asm/test.s
@@ -3,7 +3,6 @@
 	.globl _start
 
 _start:
-	addq	$0xffffffffffffffe0,%rsp
 	movq	$0x401078,%r8
 	jmp	.L0
 .L1:
@@ -20,37 +19,37 @@ _start:
 	movl	$0x1,%esi
 	xorl	%edx,%edx
 	syscall
-	movl	%eax,0x18(%rsp)
+	pushq	%rax
 	xorq	%rax,%rax
 	pushq	%rax
 	movq	$0x901f0002,%rax
 	pushq	%rax
 	movl	$0x2a,%eax
-	movl	0x28(%rsp),%edi
+	movl	0x10(%rsp),%edi
 	leaq	(%rsp),%rsi
 	movl	$0x10,%edx
 	syscall
 	movl	$1,%eax
-	movl	0x28(%rsp),%edi
+	movl	0x10(%rsp),%edi
 	movq	$0x401078,%rsi
 	movl	$0x28,%edx
 	syscall
-	movl	$0x7d,0x18(%rsp)
-	movq	$0x401a00,0x10(%rsp)
+	movl	$0x7d,0xfffffffffffffff0(%rsp)
+	movq	$0x401a00,0xfffffffffffffff8(%rsp)
 .L3:
 	xorl	%eax,%eax
-	movl	0x28(%rsp),%edi
-	movq	0x10(%rsp),%rsi
-	movl	0x18(%rsp),%edx
+	movl	0x10(%rsp),%edi
+	movq	0xfffffffffffffff8(%rsp),%rsi
+	movl	0xfffffffffffffff0(%rsp),%edx
 	syscall
 	test	%eax,%eax
 	jle 	.L2
-	addl	%eax,0x10(%rsp)
-	subl	%eax,0x18(%rsp)
+	addl	%eax,0xfffffffffffffff8(%rsp)
+	subl	%eax,0xfffffffffffffff0(%rsp)
 	jg	.L3
 .L2:
 	movl	$0x3,%eax
-	movl	0x28(%rsp),%edi
+	movl	0x10(%rsp),%edi
 	syscall
 	movl	$0x401a4c,%eax
 	call	*%rax
